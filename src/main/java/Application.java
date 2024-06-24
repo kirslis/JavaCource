7e1febb1e8505ec288f8e8a95e8c7196b6f004ef
kirslis <pav-stasik@yandex.ru>
2024-06-24 17:10:25 +0300

    ThirdTask

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,67 +1,33 @@
+import javax.imageio.ImageTranscoder;
 import java.util.Scanner;
 
 public class Application {
-    public static double getResOfFirstEquation(int x, int a, int b) {
-        double res;
-        if (x <= 3) {
-            res = b + 2 * Math.log(Math.abs(x));
-        } else {
-            res = Math.pow(x, 2) / ((double) Math.pow(x, 2) + a);
+    public static long factorial(int x) {
+        long res = x;
+
+        if (x > 1) {
+            res *= factorial(x - 1);
         }
 
         return res;
     }
 
-    public static double getResOfSecondEquation(int x, int a, int b) {
-        double res;
-        if (x <= 0) {
-            res = Math.cos(b * x + 1);
-        } else {
-            res = a + 1. / 2 * Math.exp(-x);;
-        }
-
-        return res;
-    }
-
-    public static double getResOfThirdEquation(int x, int a, int b) {
-        double res;
-        if (x <= 1) {
-            res = 1. / (Math.pow(x, 2) + Math.pow(a, 2));
-        } else {
-            res = b * Math.log(x);
-        }
+    public static double getS(int x, int n) {
+        double res = Math.pow(-1, n) * (2 * n * Math.pow(x, 2 * n + 1)) / (double) factorial(2 * n + 1);
+        if (n > 1)
+            res += getS(x, n - 1);
 
         return res;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int numberOfEquation;
-        System.out.println("Enter the number of sequence: ");
-        numberOfEquation = sc.nextInt();
-
-        int x, a, b;
-        System.out.println("Input x:");
+        int x, n;
+        System.out.print("Input N:");
+        n = sc.nextInt();
+        System.out.print("input X:");
         x = sc.nextInt();
-        System.out.println("Input a:");
-        a = sc.nextInt();
-        System.out.println("Input b:");
-        b = sc.nextInt();
 
-        double res = 0;
-        switch (numberOfEquation) {
-            case 1:
-                res = getResOfFirstEquation(x, a, b);
-                break;
-            case 2:
-                res = getResOfSecondEquation(x, a, b);
-                break;
-            case 3:
-                res = getResOfThirdEquation(x, a, b);
-                break;
-            default:
-                System.out.println("No such equation");
-        }
-        System.out.println(res);
+        System.out.println(getS(x, n));
     }
 }
